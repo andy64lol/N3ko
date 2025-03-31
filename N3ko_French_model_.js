d91e38e7317867badfbebcd68318b9edc640890e
Andy64lolxd <andy64lolxd@gmail.com>
2025-03-31 17:44:32 +0200

    Update N3ko_French_model_.js

--- N3ko_French_model_.js
+++ N3ko_French_model_.js
@@ -66,7 +66,7 @@ class NekoFrenchChat {
 
     const inputWordSet = new Set(input.words);
     const matchingWords = pattern.words.filter(word => 
-      inputWordSet.has(word)
+        inputWordSet.has(word)
     ).length;
 
     let similarity = (matchingWords / pattern.words.length) * 100;
@@ -74,11 +74,18 @@ class NekoFrenchChat {
     const regex = new RegExp(pattern.words.join('.*'), 'i');
     const regexMatch = input.normalized.match(regex);
     if (regexMatch) {
-      similarity = Math.max(similarity, 65); 
+        similarity = Math.max(similarity, 65); 
+    }
+
+    if (
+        pattern.normalized.includes(input.normalized) ||
+        input.normalized.includes(pattern.normalized)
+    ) {
+        similarity = 100;
     }
 
     return similarity;
-  }
+}
   
   findMatchingIntent(userInput) {
     const processedInput = this.processInput(userInput);
